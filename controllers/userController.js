4144005522f1e6d78a50dde8c6004c6a59dc6992
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-12-01 13:30:52 -0600

    Add registration view and update user controller for login and register routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
-const formLogin = (req, res) => {
-  res.render("auth/login", {
-    auth: true,
-  });
+export const formLogin = (req, res) => {
+  res.render("auth/login", {});
 };
 
-export { formLogin };
+export const formRegister = (req, res) => {
+  res.render("auth/register", {});
+};
