825271c900c9954ee3aa9f84ad3c305a4b33ce9b
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-12-02 21:05:08 -0600

    Add validation for user registration fields and integrate express-validator

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+import { check, validationResult } from "express-validator";
 import User from "../models/User.js";
 
 export const formLogin = (req, res) => {
@@ -13,6 +14,27 @@ export const formRegister = (req, res) => {
 };
 
 export const registerUser = async (req, res) => {
+  await check("name")
+    .notEmpty()
+    .withMessage("El campo nombre es obligatorio")
+    .run(req);
+  await check("email").isEmail().withMessage("El email no es válido").run(req);
+  await check("password")
+    .isLength({ min: 6 })
+    .withMessage("La contraseña debe tener al menos 6 caracteres")
+    .run(req);
+  await check("password_confirmation")
+    .equals("password")
+    .withMessage("Las contraseñas no coinciden")
+    .run(req);
+
+  let result = validationResult(req);
+
+  if (!result.isEmpty()) {
+  }
+
+  res.json(result.array());
+
   const user = await User.create(req.body);
 
   res.json(user);
